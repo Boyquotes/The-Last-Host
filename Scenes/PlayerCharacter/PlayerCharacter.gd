37967cd099a38bd9d472389c93637db950c93470
Maaack <marek.belski@gmail.com>
2023-04-16 16:27:18 -0700

    Adds player movement.

---
+++ Scenes/PlayerCharacter/PlayerCharacter.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+
+@export var acceleration = 600
+@export var max_speed = 125
+@export var friction = 600
+
+func move_state(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	input_vector.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		velocity = velocity.move_toward(input_vector * max_speed, acceleration * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+	move()
+
+func move():
+	move_and_slide()
+
+func _physics_process(delta):
+	move_state(delta)
+

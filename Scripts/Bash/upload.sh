315d5a8488acb31be053f5e1391b3f6384641acb
Maaack <marek.belski@gmail.com>
2023-04-16 17:35:15 -0700

    Adds export bash script.

---
+++ Scripts/Bash/upload.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# butler upload commands
+
+butler push ./HTML5/ 'maaack/the-last-host:html5'
+butler push ./Linux/ 'maaack/the-last-host:linux'
+butler push ./MacOSX/ 'maaack/the-last-host:osx'
+butler push ./Windows/ 'maaack/the-last-host:win'

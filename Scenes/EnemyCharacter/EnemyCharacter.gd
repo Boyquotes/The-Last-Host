01fe8009d6a290743354ce72a101bdb2d80213f5
tomy204 <tomy5924@gmail.com>
2023-04-19 15:55:49 +0200

    enemies have now health and can be killed

--- Scenes/EnemyCharacter/EnemyCharacter.gd
+++ Scenes/EnemyCharacter/EnemyCharacter.gd
@@ -2,9 +2,11 @@ extends CharacterBody2D
 
 signal damage_taken(current_position : Vector2, damage_amount : float)
 
+@export var max_health = 100
 @export var movement_speed = 10
 
 var player_node : CharacterBody2D
+var health = max_health
 
 
 func _physics_process(delta):
@@ -16,5 +18,14 @@ func _physics_process(delta):
 	move_and_slide()
 
 func hit(damage_amount):
-	$AnimationPlayer.play("HitFlash")
+	health -= damage_amount
+	if health > 0:
+		$AnimationPlayer.play("HitFlash")
+	else:
+		$AnimationPlayer.play("FadeOut")
 	emit_signal("damage_taken", position, damage_amount)
+
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "FadeOut":
+		queue_free()
